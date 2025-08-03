d7895935807e330bc68ff09ed07e74c8bfdd2061
Juergen Schilling <juergen_schilling@web.de>
2025-08-03 22:11:57 +0200

    Add entry point for loading nodes and credentials from the dist directory

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// This file is the entry point for n8n to discover the node
+// It loads the compiled JavaScript code from the dist directory
+
+// Import from the compiled dist directory
+const { nodes, credentials } = require('./dist');
+
 module.exports = {
 	nodes: [
 		{
